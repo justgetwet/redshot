054483312ef522fb96a0eda8c651a09d21b66d65
AkiraWin <frog7bar@gmail.com>
2021-11-14 00:18:15 +0900

    fix vendor/bundle

--- redshotinit.rb
+++ redshotinit.rb
@@ -1,9 +1,9 @@
 require "fileutils"
 
-p Dir.glob("vendor/ruby/2.7.0/bundler/gems/*")
+p Dir.glob("vendor/bundle/ruby/2.7.0/bundler/gems/*")
 
 ["_config.yml", "index.md", "404.md"].each{|filename|
-  path = "vendor/ruby/2.7.0/bundler/gems/redshot-*/" + filename
+  path = "vendor/bundle/ruby/2.7.0/bundler/gems/redshot-*/" + filename
   FileUtils.cp(Dir.glob(path)[0], filename)
 }
 
@@ -14,19 +14,19 @@ FileUtils.mkdir_p("#{posts}") unless File.exists?("#{posts}")
 =begin
 docs/_site
 docs/.jekyll-cache
-docs/vendor/ruby/2.7.0/bin
-docs/vendor/ruby/2.7.0/build_info
-docs/vendor/ruby/2.7.0/cache
-docs/vendor/ruby/2.7.0/doc
-docs/vendor/ruby/2.7.0/extensions
-docs/vendor/ruby/2.7.0/gems
-docs/vendor/ruby/2.7.0/specifications
+docs/vendor/bundle/ruby/2.7.0/bin
+docs/vendor/bundle/ruby/2.7.0/build_info
+docs/vendor/bundle/ruby/2.7.0/cache
+docs/vendor/bundle/ruby/2.7.0/doc
+docs/vendor/bundle/ruby/2.7.0/extensions
+docs/vendor/bundle/ruby/2.7.0/gems
+docs/vendor/bundle/ruby/2.7.0/specifications
 =end
 
 # .gitmodules
 =begin
-[submodule "docs/vendor/ruby/2.7.0/bundler/gems/redshot-f28e5c796238"]
-  path = docs/vendor/ruby/2.7.0/bundler/gems/redshot-f28e5c796238
+[submodule "docs/vendor/bundle/ruby/2.7.0/bundler/gems/redshot-f28e5c796238"]
+  path = docs/vendor/bundle/ruby/2.7.0/bundler/gems/redshot-f28e5c796238
   url = https://github.com/justgetwet/redshot.git
   branch = main
 =end
